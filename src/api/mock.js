b0ceccd74aaf86e23d541bc2952b55caf9220cd2
xinyulu <xinyulu3344@163.com>
2022-04-06 23:27:51 +0800

    user add edit api mock

--- src/api/mock.js
+++ src/api/mock.js
@@ -1,4 +1,7 @@
 import Mock from 'mockjs'
 import homeApi from '../mock/home'
+import userApi from '../mock/user'
 
 Mock.mock('/home/getData', homeApi.getStatisticalData)
+Mock.mock('/api/user/add', 'post', userApi.createUser)
+Mock.mock('/api/user/edit', 'post', userApi.updateUser)
--- src/main.js
+++ src/main.js
@@ -21,7 +21,15 @@ import {
   TableColumn,
   Breadcrumb,
   BreadcrumbItem,
-  Tag
+  Tag,
+  Form,
+  FormItem,
+  Input,
+  Select,
+  Option,
+  Switch,
+  DatePicker,
+  Dialog
 } from "element-ui";
 
 import "element-ui/lib/theme-chalk/index.css";
@@ -53,6 +61,14 @@ Vue.use(TableColumn)
 Vue.use(Breadcrumb)
 Vue.use(BreadcrumbItem)
 Vue.use(Tag)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Switch)
+Vue.use(DatePicker)
+Vue.use(Dialog)
 
 Vue.prototype.$http = http
 
